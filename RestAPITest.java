63358b93d7894698e012909deafe3adaaa2fae3a
dgotlieb <Dgotlieb8@gmail.com>
2022-07-22 12:19:18 +0300

    Update RestAPITest.java

--- RestAPITest.java
+++ RestAPITest.java
@@ -1,5 +1,6 @@
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,18 +10,20 @@ import java.io.IOException;
 public class RestAPITest {
     public static void main(String[] args) throws IOException, JSONException {
         // use OKHttp client to create the connection and retrieve data
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
         Request request = new Request.Builder()
-                .url("http://api.exchangeratesapi.io/v1/latest?access_key=XXXXXXXXXXXX&symbols=USD,ILS")
+                .url("https://api.apilayer.com/exchangerates_data/convert?to=ILS&from=USD&amount=1")
+                .addHeader("apikey", "XXXXXXXXXXXXXXXX")
                 .build();
         Response response = client.newCall(request).execute();
         String jsonData = response.body().string();
+        response.close();
         // parse JSON
         JSONObject mainJsonObject = new JSONObject(jsonData);
         // get Json object
-        JSONObject resultsJson = mainJsonObject.getJSONObject("USD_ILS");
+        JSONObject resultsJson = mainJsonObject.getJSONObject("info");
         // get value
-        double val = resultsJson.getDouble("val");
+        double val = resultsJson.getDouble("rate");
         System.out.println(val);
     }
 }
